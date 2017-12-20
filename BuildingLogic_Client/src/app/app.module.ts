97cd7c99a869cff340749d4ca7848386749975a6
birukwmichael@gmail.com <birukwmichael@gmail.com>
2017-12-20 14:29:07 +0300

    Angular Admin LTE  Initial Ingegration

--- BuildingLogic_Client/src/app/app.module.ts
+++ BuildingLogic_Client/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from './layout/footer.component';
+import { MenuComponent } from './layout/menu.component';
+import { SetttingsComponent } from './layout/setttings.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SetttingsComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule
